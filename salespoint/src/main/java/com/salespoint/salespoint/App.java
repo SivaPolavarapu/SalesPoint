a23bb06e06a5df31dfa4acb5ed0b950c3709126e
a633869 <a633869@V452122.dmn1.fmr.com>
2018-09-07 13:39:21 -0400

    Fix case on initial greeting

--- salespoint/src/main/java/com/salespoint/salespoint/App.java
+++ salespoint/src/main/java/com/salespoint/salespoint/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Hello everybody again");
+        System.out.println("Sales data");
         System.out.println("ujhdfksd");
         System.out.println("Gaja");
     }
