aa8899301bdd7d6849a8f97ccb2efbcd00544e88
a633869 <a633869@V452122.dmn1.fmr.com>
2018-09-07 13:47:54 -0400

    Lemon Rice

--- salespoint/src/main/java/com/salespoint/salespoint/SalesData.java
+++ salespoint/src/main/java/com/salespoint/salespoint/SalesData.java
@@ -5,7 +5,7 @@ public class SalesData {
 	int data[] = {0, 4,42};
 	 public void display() {
 		 for(int i=0;i<data.length;i++) {
-			 System.out.println("");
+			 System.out.println("pulihora");
 		 }
 	 }
 }
