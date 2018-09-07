b8c2b19b37552b0f529ed3a0161ca7ba1a6094d1
a633869 <a633869@V452122.dmn1.fmr.com>
2018-09-07 12:01:48 -0400

    committed

---
+++ salespoint/src/main/java/com/salespoint/salespoint/App.java
@@ -0,0 +1,14 @@
+package com.salespoint.salespoint;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hello everybody again");
+    }
+}
